7850272b529d709545650d6608bec5a06c2f1688
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-03-09 11:15:15 -0300

    sistema de login,cadastro e mensagem funcionando

---
+++ backend/routes/acesso.js
@@ -0,0 +1,55 @@
+import express from "express"
+import db from "./db.js"
+import jwt from "jsonwebtoken"
+import segredo from "./segredo.js"
+
+
+const roteador = express.Router()
+
+async function login(req, res) {
+    const { email, senha } = req.body;
+
+    const [encontrado] = await db.query(
+        "SELECT * FROM usuarios WHERE email = ? AND senha = ?",
+        [email, senha]
+    );
+
+    if (encontrado.length > 0) {
+
+        const usuario = encontrado[0];
+
+        const token = jwt.sign(
+            { id: usuario.id },
+            "segredo",
+            { expiresIn: "1h" }
+        )
+        res.status(200).json({
+            mensagem: "login valido",
+            token: token
+        });
+    } else {
+        res.status(401).json({ mensagem: "Usuario ou senha invalido" });
+    }
+}
+
+async function cadastrar(req, res) {
+    try {
+        const { email, senha, nome } = req.body
+        const [resultado] = await db.query("INSERT INTO usuarios(nome,email,senha) VALUES(?,?,?)", [nome, email, senha])
+        if (resultado.affectedRows > 0) {
+            return res.status(201).json({ message: "Cadastrado com sucessso" })
+        } else {
+            return res.status(401).json({ message: "Erro ao cadastrar" })
+        }
+    } catch (erro) {
+        console.error(erro)
+        if (erro.code === "ER_DUP_ENTRY") {
+            res.status(400).json({ message: "Email ou nome de usuario indisponivel" })
+        }
+    }
+}
+
+roteador.post("/login", login);
+roteador.post("/cadastro", cadastrar)
+
+export default roteador
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import rotaCadastro from "./routes/cadastro.js"
-import rotaLogin from "./routes/login2.js"
+import rotaLogin from "./routes/acesso.js"
 import rotaMensagem from "./routes/mensagem.js"
-import jwt from "jsonwebtoken"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(rotaCadastro);
 app.use(rotaLogin);
 app.use(rotaMensagem);
 
