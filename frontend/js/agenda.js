e6003a2280039f944a256a40211ff3f48598803c
Vinicius Santos Camelo <vini976964150@gmail.com>
2026-03-13 22:27:27 -0300

    arrumei o agenda e fiz uma Base

--- frontend/js/agenda.js
+++ frontend/js/agenda.js
@@ -1,7 +1,8 @@
 const apa = document.getElementById("aparece");
 const cada = document.getElementById("cadastroal");
 
-apa.addEventListener('click', () => {
+apa.addEventListener('click', function() {
+  cada.classList.remove("desaparece");
   cada.innerHTML = `
         <div class="al">
           <h2>Cadastrar Novo Aluno</h2>
@@ -9,25 +10,25 @@ apa.addEventListener('click', () => {
         <div class="c1">
           <div>
             <label for="nome">NOME</label><br>
-            <input type="text" name="nome" class="nome" placeholder="Nome do Aluno">
+            <input type="text" id="nome" class="nome" placeholder="Nome do Aluno">
           </div>
           <div>
             <label for="turma">TURMA</label><br>
-            <input type="text" name="turma" class="turma" placeholder="Ex:3DM">
+            <input type="text" id="turma" class="turma" placeholder="Ex:3DM">
           </div>
           <div>
             <label for="ra">RA</label><br>
-            <input type="text" name="ra" class="ra" placeholder="Ex:00011....">
+            <input type="text" id="ra" class="ra" placeholder="Ex:00011....">
           </div>
         </div>
         <div class="c1">
           <div>
             <label for="forte">🌟 PONTOS FORTES</label><br>
-            <input type="text" name="forte" class="forte" placeholder="Descreva os pontos forte do Aluno ">
+            <input type="text" id="forte" class="forte" placeholder="Descreva os pontos forte do Aluno ">
           </div>
           <div>
             <label for="fraco">⚠️ PONTOS FRACO</label><br>
-            <input type="text" name="fraco"  class="fraco" placeholder="Descreva os pontos fraco do Aluno ">
+            <input type="text" id="fraco"  class="fraco" placeholder="Descreva os pontos fraco do Aluno ">
           </div>
         </div>
         <button>Cadastrar Aluno</button>
