540aa858696e64b0d8c6c65ba78898cf1ce1c43a
vinicius <vini976964150@gmail.com>
2026-03-20 10:29:17 -0300

    loginn

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -4,9 +4,9 @@ import mysql from 'mysql2/promise';
 // dotenv.config();
 
 const db = mysql.createPool({
-    host: '171.30.1.232',
-    user: 'back',
-    password: "rrnea.@2009",
+    host: '172.30.1.232',
+    user: 'vinicius',
+    password: "senhavini",
     database: 'app_db',
     port: '3306'
 })
